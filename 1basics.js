6b6fa4e461726a8e851d96baa8ea4ee661c8c707
Abhishek <theabhishek.srm@gmail.com>
2024-06-14 11:02:27 +0530

    strings

---
+++ 1basics.js
@@ -0,0 +1,52 @@
+
+// let str1 = "Ram is a good"
+// let str2 =  'Hello world'
+// let str3 = `Hello world`
+
+
+// let a = 10 
+
+// let ans = str1 + a
+
+// let str4 = "Hello world " + a
+
+// let str5 = `Hello world ${a}`
+
+
+// let str = "Ram is \
+// dancing on floor\
+// and he is enjoying it"
+
+// let str = `Ram 
+// is dancing
+//     on The floor
+// `
+
+// console.log(str)
+
+
+// let str = "abhishek" 
+
+// console.log(str.length) // 8 
+
+// console.log(str[0]) // a 
+// console.log("n1",str[10]) 
+
+// console.log("n2",str.charAt(10)) // a
+
+// loops: 
+
+// for(let i = 0; i <= str.length-1; i++){
+//     console.log(str[i])
+// }
+
+// str = "abhishek"
+
+// for(let t of str){
+//     console.log(t)
+// }
+
+
+// for(let t in str){
+//     console.log(t)
+// }
---
+++ 2.string.js
@@ -0,0 +1,38 @@
+
+
+// let str = "papa"
+
+// str[0] = "k" 
+
+// str = "ramesh"
+
+// console.log(str) 
+
+// strings => immutable => cannot be changed
+
+// str1 = str
+
+
+// let str1 = str.replace("p", "k")
+
+// console.log("old",str)
+// console.log("new",str1)
+
+// console.log(str.replaceAll("p", "k"))
+
+
+
+let str = "abhishek"
+
+// str.rev()
+
+// lets reverse this string 
+// "kehsihba"
+
+
+let rev = ""
+for(let i = str.length-1; i >= 0; i--){
+     rev = rev + str[i]
+}
+
+console
